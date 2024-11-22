962f42afa815798cdf95054084e6a1a9e6b18696
ngocnt512 <uhannhien@gmail.com>
2024-11-22 22:10:14 +0700

    'Noc

---
+++ AutoKid_v2/AutoKid/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(authz -> authz.anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/admin/login").permitAll()
+                        .loginProcessingUrl("/login")
+                        .usernameParameter("email")
+                        .defaultSuccessUrl("/admin/staff/home", true)
+                        .failureUrl("/admin/login?error=true")
+                )
+
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/admin/login")
+                        .permitAll()
+                )
+                //cấu hình session
+                .sessionManagement()
+                .invalidSessionUrl("/admin/login")
+                .maximumSessions(1)
+                .expiredUrl("/admin/login");
+
+
+        http.csrf().disable();
+        return http.build();
+    }
+
+}
