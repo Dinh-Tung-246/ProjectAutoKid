3d1c3033373bcc6b3264675781ebb6448eabf222
Dinh-Tung-246 <145095729+Dinh-Tung-246@users.noreply.github.com>
2024-12-10 14:44:06 +0700

    update gen cart from db

--- AutoKid_v2/AutoKid/src/main/java/com/example/demo/controller/AutokidShoppingCartController.java
+++ AutoKid_v2/AutoKid/src/main/java/com/example/demo/controller/AutokidShoppingCartController.java
@@ -1,15 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.GioHangChiTiet;
 import com.example.demo.repository.LoaiSanPhamRepo;
+import com.example.demo.response.GioHangChiTietResponse;
 import com.example.demo.service.QuanLyGioHangService;
 import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -17,6 +18,8 @@ import java.util.Map;
 @Controller
 @RequestMapping("/autokid/shoping-cart")
 public class AutokidShoppingCartController {
+    private static Logger logger = LoggerFactory.getLogger(AutokidShoppingCartController.class);
+
     @Autowired
     LoaiSanPhamRepo loaiSanPhamRepo;
 
@@ -24,12 +27,17 @@ public class AutokidShoppingCartController {
     QuanLyGioHangService qlghService;
 
     @GetMapping("")
-    public String showShoppingCart(@RequestParam(required = false) Integer idKH, Model model){
-        if (idKH != null) {
-            model.addAttribute("cartItems", qlghService.getGioHang(idKH));
-        }
+    public String showShoppingCart(Model model){
         model.addAttribute("loaisp", loaiSanPhamRepo.findAll());
         model.addAttribute("currentPage","shoping-cart");
         return "/autokid/shoping-cart";
+    }
+
+    @PostMapping("/get-cart-from-db")
+    @ResponseBody
+    public List<GioHangChiTietResponse> getCartFromDB(@RequestBody Map<String, Object> idKH) {
+        Integer idKHInt = Integer.parseInt(idKH.get("idKH").toString());
+        List<GioHangChiTietResponse> list = qlghService.getGioHang(idKHInt);
+        return list;
     }
 }
