264f923fd8ed83fcfb0b749a9de58285919b9b6e
milo16 <tp9023819@gmail.com>
2024-12-11 23:26:29 +0700

    fix trùng

--- AutoKid_v2/AutoKid/src/main/java/com/example/demo/repository/NhanVienRepo.java
+++ AutoKid_v2/AutoKid/src/main/java/com/example/demo/repository/NhanVienRepo.java
@@ -14,7 +14,6 @@ public interface NhanVienRepo extends JpaRepository<NhanVien,Integer> {
     NhanVien findByMaNV(String maNv);
     List<NhanVien> findByTenNVContainingOrMaNVContaining(String tenNV, String maNV);
 
-    NhanVien findByMaNV(String maNv);
 
     boolean existsByMaNV(String maNV);
 }
